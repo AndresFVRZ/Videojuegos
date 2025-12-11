9343e96bc30ac02157c93d439124ecec383228a6
AndresFVRZ <vergararoblesandresfelipe204@gmail.com>
2025-12-11 02:18:09 -0500

    solucion de error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { Home } from './page/home/home';
 import { About } from './page/about/about';
 import { Contact } from './page/contact/contact';
 import { RequisitosComponent } from './page/requisitos/requisitos';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: Home },
   { path: 'nosotros', component: About },
   { path: 'contacto', component: Contact },
   { path: 'requisitos', component: RequisitosComponent },
-
   { path: '**', redirectTo: '' },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
